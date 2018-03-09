ffc2faa35774a2546f583f65be85ce9a71a9246f
FilipVrt <filip.vartolomei@gmail.com>
2018-03-09 13:36:06 +0100

    made an exaple using StarRating component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import logo from './logo.svg';
 import StarRating from './StarRating/StarRating';
 
 class App extends Component {
   state = {
-    width: 22
+    feedbackItems: {
+      shippment: false,
+      description: false,
+      comunications: false
+    }
   };
   onInputChange = (e) => {
     console.log(e.target.value);
@@ -13,48 +18,74 @@ class App extends Component {
     });
   }
   onRatingComplete = (whatRated) => {
+    const feedbackItems = {...this.state.feedbackItems};
+    feedbackItems[whatRated.name] = whatRated.rate;
+    this.setState({feedbackItems});
     console.log(whatRated);
   }
   render() {
+    const feedback = this.state.feedbackItems;
+    const buttonDisabled = (feedback.comunications > 0 && feedback.description > 0 && feedback.shippment > 0);
     return (
       <div className="App">
-        <div className="rating">
-          <div className="rating__description">
-            <p>Shippment time</p>
-          </div>
-          <StarRating 
-            width={this.state.width}
-            name={'stocazzo1'}
-            onRatingComplete={this.onRatingComplete}
-          />
-        </div>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Rating Star <i>Component</i> 
+            <div className="component">
+              <StarRating
+                selectedStar={5}
+                name={'header-star'}
+                onRatingComplete={this.onRatingComplete}
+              /> 
+            </div>
+          in React</h1>
+        </header>
 
-        <div className="rating">
-          <div className="rating__description">
-            <p>Item descritpion</p>
+        <h1 className="title__page">Items Bought Feedback</h1>
+        <div className="rate-product">
+          <div className="product">
+            <img src={logo} className="product__img" alt="logo" />
+            <div className="product__info">
+              <h3 className="product__name">React Stickers</h3>
+              <p className="product__description">React Sticker to apply on your MacBook or on your preferable device...</p>
+              <p className="product__bought__date">Bought on: {new Date().toLocaleDateString()}</p>
+            </div>
           </div>
-          <StarRating 
-            width={this.state.width}
-            selectedStar={3}
-            titlesCustomText={['uno', 'due', 'tre', 'quattro', 'cinque']}
-            name={'stocazzo2'}
-            onRatingComplete={this.onRatingComplete}
-          />
-        </div>
 
-        <div className="rating">
-          <div className="rating__description">
-            <p>Comunications</p>
+          <div className="rating">
+            <div className="rating__description">
+              <p>Shippment time</p>
+            </div>
+            <StarRating
+              name={'shippment'}
+              onRatingComplete={this.onRatingComplete}
+            />
           </div>
-          <StarRating 
-            width={this.state.width}
-            selectedStar={3}
-            titlesCustomText={['uno', 'due', 'tre', 'quattro', 'cinque']}
-            name={'stocazzo3'}
-            onRatingComplete={this.onRatingComplete}
-          />
-        </div>
 
+          <div className="rating">
+            <div className="rating__description">
+              <p>Item descritpion</p>
+            </div>
+            <StarRating
+              selectedStar={3}
+              titlesCustomText={['one', 'two', 'three', 'four', 'five']}
+              name={'description'}
+              onRatingComplete={this.onRatingComplete}
+            />
+          </div>
+
+          <div className="rating">
+            <div className="rating__description">
+              <p>Comunications</p>
+            </div>
+            <StarRating
+              name={'comunications'}
+              onRatingComplete={this.onRatingComplete}
+            />
+          </div>
+
+          <button className={'btn btn-blue ' + (buttonDisabled ? 'enabled' : 'disabled')}>Submit Feedback</button> 
+        </div>
       </div>
     );
   }
